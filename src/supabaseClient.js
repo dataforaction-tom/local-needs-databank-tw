18910b942c97f9dbde890b7d3c4c53bcaad85627
Tom C Watson <150357824+dataforaction-tom@users.noreply.github.com>
2024-04-19 09:06:23 +0100

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-console.log(supabaseUrl);
+console.log('Api called')
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-console.log(supabaseAnonKey);
+
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
